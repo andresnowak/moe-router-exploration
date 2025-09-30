264d9993ffa17c0aa536e544ce9d5e15b7f4a535
Andres Nowak <andresnowak52@gmail.com>
2025-09-30 22:15:11 +0200

    Added runnning scripts

---
+++ run_gpt_oss.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#SBATCH --job-name=gpt_oss_router_statistics
+#SBATCH --time=05:00:00
+#SBATCH --nodes=1                # total number of nodes
+#SBATCH --ntasks-per-node=1      # total number of tasks per node
+#SBATCH --gpus-per-task=1
+#SBATCH --output=logs/slurm-%x-%j.log  # if #SBATCH --error=... is not specified,
+                                 # this will also contain stderr (error messages)
+
+# Initialization.
+export UV_LINK_MODE=copy
+export UV_CACHE_DIR=$SCRATCH/uv_cache
+
+export HF_CACHE=/$SCRATCH/hf_cache
+export TRANSFORMERS_CACHE=/$SCRATCH/hf_cache
+
+srun --environment=pytorch2506 bash -c "
+
+cd $HOME/developer/moe-router-exploration
+
+if [[ $(uname -m) == "aarch64" ]]; then
+    source venv-arm/bin/activate
+else
+    source venv-amd/bin/activate
+fi
+
+source set_threads.sh
+
+
+python main.py \
+    --model_name "openai/gpt-oss-20b" \
+    --out_data_dir "$SCRATCH/moe-router-exploration-data" \
+    --data_name "cais/mmlu" \
+
+python main.py \
+    --model_name "openai/gpt-oss-20b" \
+    --out_data_dir "$SCRATCH/moe-router-exploration-data" \
+    --data_name "TIGER-Lab/MMLU-Pro" \
+
+python main.py \
+    --model_name "openai/gpt-oss-20b" \
+    --out_data_dir "$SCRATCH/moe-router-exploration-data" \
+    --data_name "openai/MMMLU" \
+"
